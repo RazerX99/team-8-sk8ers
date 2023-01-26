0860b663c105b357a35a2838c525ddfa874f447c
Meri K <merikhurshudyan52@gmail.com>
2023-01-26 15:17:13 +0000

    first and some of the rest

--- src/test/java/com/levelup/forestsandmonsters/features/GameSteps.java
+++ src/test/java/com/levelup/forestsandmonsters/features/GameSteps.java
@@ -28,4 +28,40 @@ public class GameSteps {
         assertEquals(expectedName, gc.getStatus().characterName);
     }
 
+    @Given("the character starts at position with XCoordinates {int}")
+    public void the_character_starts_at_position_with_x_coordinates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("starts at YCoordinates {int}")
+    public void starts_at_y_coordinates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the player choses to move in North")
+    public void the_player_choses_to_move_in_north() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the character moves")
+    public void the_character_moves() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the character is now at position with XCoordinates {int}")
+    public void the_character_is_now_at_position_with_x_coordinates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("YCoordinates {int}")
+    public void y_coordinates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
---
+++ src/test/resources/com/levelup/forestsandmonsters/features/move.feature
@@ -0,0 +1,16 @@
+Feature: Move in a direction
+
+I want to move my character. If they attempt to move past a boundary, the move resutls in no change in position.
+
+    Scenario Outline: Move in a direction 
+        Given the character starts at position with XCoordinates <startingPositionX>
+        And starts at YCoordinates <startingPositionY>
+        And the player choses to move in <direction>
+        When the character moves
+        Then the character is now at position with XCoordinates <endPositionX>
+        And YCoordinates <endPositionY>
+        Examples:
+        |startingPositionX|startingPositionY|direction|endPositionX|endPositionY|
+        |0|0|North|0|1|
+
+    
